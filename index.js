ea1b38348fba439cb354862f9ea50bc95ab5b19a
andybro <132149967+mrlit-a@users.noreply.github.com>
2023-08-29 11:50:04 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-import { join, dirname } from 'path'
+import { join, dirname } from "path";
 import { createRequire } from "module";
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
-import chalk from "chalk"
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) 
-const { name, author } = require(join(__dirname, './package.json')) 
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
+import { fileURLToPath } from "url";
+import { setupMaster, fork } from "cluster";
+import { watchFile, unwatchFile } from "fs";
+import cfonts from "cfonts";
+import chalk from "chalk";
+import { createInterface } from "readline";
+import yargs from "yargs";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const { name, author } = require(join(__dirname, "./package.json"));
+const { say } = cfonts;
+const rl = createInterface(process.stdin, process.stdout);
 
 say('Gata\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por Gata Dios | DIEGO-OFC`, {
+say(`Project Author:\nGataNina-Li (@gata_dios)\n\nColaboradores:\nelrebelde21 (Mario)\nAzamiJs (Azami)`, {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']})
+gradient: ['red', 'magenta']});
 
-var isRunning = false
+var isRunning = false;
 /**
 * Start a js file
 * @param {String} file `path/to/file`
 */
 function start(file) {
 if (isRunning) return
-isRunning = true
+isRunning = true;
 let args = [join(__dirname, file), ...process.argv.slice(2)]
   
 setupMaster({
 exec: args[0],
-args: args.slice(1), })
+args: args.slice(1),
+})
 let p = fork()
 p.on('message', data => {
+//console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
 switch (data) {
 case 'reset':
 p.process.kill()
@@ -45,23 +47,22 @@ start.apply(this, arguments)
 break
 case 'uptime':
 p.send(process.uptime())
-break }})
+break
+}})
 p.on('exit', (_, code) => {
 isRunning = false
 console.error('⚠️ Error Inesperado ⚠️', code)
-  
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-  
-if (process.env.pm_id) {
-process.exit(1)
-} else {
-process.exit()
-}
+if (code === 0) return
+watchFile(args[0], () => {
+unwatchFile(args[0])
+start(file)
+})
 })
 let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
 if (!opts['test'])
 if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())})}
+p.emit('message', line.trim())
+})
+//console.log(p)
+}
 start('main.js')
